fea57f65d44d45f816ce5976265c3030ca044652
Paulo Santos <paulo_sss@hotmail.com>
2020-05-14 17:28:38 +0100

    handling post requests

--- 04_natours/app.js
+++ 04_natours/app.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -22,6 +24,25 @@ app.get('/api/v1/tours', (req, res) => {
     results: tours.length,
     data: tours,
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours.slice(-1)[0].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'sucess',
+        data: { tour: newTour },
+      });
+    }
+  );
 });
 
 const PORT = 3000;
