910398b9f45db1c9e511b224ba0effb2b6430013
Paulo Santos <paulo.santos@kentpharm.co.uk>
2020-06-22 16:09:56 +0100

    setting up express and basic routing

---
+++ 04_natours/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res
+    .status(404)
+    .json({ message: 'Hello from the server side', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint...');
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}...`);
+});
