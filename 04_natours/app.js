07372c37c841152fbcdb3a09ade4fd038f05e31b
Paulo Santos <paulo.santos@kentpharm.co.uk>
2020-06-24 15:42:50 +0100

    write own middleware

--- 04_natours/app.js
+++ 04_natours/app.js
@@ -8,13 +8,25 @@ const app = express();
 // use middleware
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'sucess',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: { tours },
   });
