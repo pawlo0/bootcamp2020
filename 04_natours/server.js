8422dec82477b067b77df650dd7a02636a65f6b3
Paulo Santos <paulo.santos@kentpharm.co.uk>
2020-07-01 14:01:22 +0100

    connect to mongodb

--- 04_natours/server.js
+++ 04_natours/server.js
@@ -1,6 +1,18 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
 dotenv.config({ path: './config.env' })
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections)
+    console.log('DB connection successuful!')
+  })
 
 const app = require('./app')
 
