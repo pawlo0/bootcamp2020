6be6509328d6966c2bb00ca0e4db1fddd563ffa0
Paulo Santos <paulo.santos@kentpharm.co.uk>
2020-07-02 09:08:13 +0100

    create and read documents

--- 04_natours/controllers/tourController.js
+++ 04_natours/controllers/tourController.js
@@ -1,47 +1,55 @@
 const Tour = require('../models/tourModel')
 
-exports.checkBody = (req, res, next) => {
-  console.log(req.body)
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find()
+
+    res.status(200).json({
+      status: 'sucess',
+      results: tours.length,
+      data: { tours },
+    })
+  } catch (err) {
+    res.status(404).json({
       status: 'fail',
-      message: 'Missing name or price',
+      message: err,
     })
   }
-  next()
 }
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime)
-  res.status(200).json({
-    status: 'sucess',
-    requestedAt: req.requestTime,
-    // results: tours.length,
-    // data: { tours },
-  })
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id)
+    res.status(200).json({
+      status: 'sucess',
+      data: {
+        tour,
+      },
+    })
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    })
+  }
 }
 
-exports.getTour = (req, res) => {
-  console.log(req.params)
-  const id = req.params.id * 1
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body)
 
-  // const tour = tours.find((el) => el.id === id)
-
-  // res.status(200).json({
-  //   status: 'sucess',
-  //   data: {
-  //     tour,
-  //   },
-  // })
-}
-
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'sucess',
-    // data: {
-    //   tour: newTour,
-    // },
-  })
+    res.status(201).json({
+      status: 'sucess',
+      data: {
+        tour: newTour,
+      },
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    })
+  }
 }
 
 exports.updateTour = (req, res) => {
