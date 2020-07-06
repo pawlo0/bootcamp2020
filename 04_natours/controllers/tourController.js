4a84afe933e3a14fe6a43f4d8175dc741207654b
Paulo Santos <paulo.santos@kentpharm.co.uk>
2020-07-06 13:12:22 +0100

    sorting

--- 04_natours/controllers/tourController.js
+++ 04_natours/controllers/tourController.js
@@ -3,19 +3,24 @@ const Tour = require('../models/tourModel')
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
-    // 1) filtering
+    // 1A) filtering
     const queryObj = { ...req.query }
     const excludedFields = ['page', 'sort', 'limit', 'fields']
     excludedFields.forEach((el) => delete queryObj[el])
 
-    // 2) Advance filtering
+    // 1B) Advance filtering
     let queryStr = JSON.stringify(queryObj)
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
-    console.log(JSON.parse(queryStr))
 
-    const query = Tour.find(JSON.parse(queryStr))
+    let query = Tour.find(JSON.parse(queryStr))
 
-    // const query = Tour.find().where('duration').equals(5).where('difficulty').equals('easy')
+    // 2) SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ')
+      query = query.sort(sortBy)
+    } else {
+      query = query.sort('-createdAt')
+    }
 
     // EXECUTE QUERY
     const tours = await query
